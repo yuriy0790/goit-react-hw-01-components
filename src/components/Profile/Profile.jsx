1de86cd619e80302338649b945d3813b920ee67e
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-12-12 21:47:10 +0200

    profile + statistics

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import defaultImg from './defaultImg.jpg';
+
+export default function Profile({
+  username,
+  tag,
+  location,
+  avatar = defaultImg,
+  stats,
+}) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" width="200px" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,16 @@
+export default function Profile({ title, stats }) {
+  return (
+    <section class="statistics">
+      {title && <h2 class="title">{title}</h2>}
+
+      <ul class="stat-list">
+        {stats.map(stat => (
+          <li class="item" key={stat.id}>
+            <span class="label">{stat.label}</span>
+            <span class="percentage">{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
